4daf5aa6c191f1f3fa8e33d2b31da948128fff7b
Dasari satyam <dasarisatyam@Dasaris-MacBook-Pro.local>
2024-07-17 18:30:53 +0530

    Added ability to pass tokens when creating instance

--- src/CcAvenue.php
+++ src/CcAvenue.php
@@ -6,40 +6,43 @@ use Satyam147\LaravelCcavenue\Utils\Crypto;
 
 class CcAvenue
 {
-    private $productionUrl = 'https://secure.ccavenue.com';
-    private $sandboxUrl = 'https://test.ccavenue.com';
-    private RequestData $paymentData;
-    public function __construct(RequestData $paymentData)
-    {
-        $this->paymentData = $paymentData;
+    private string $productionUrl = 'https://secure.ccavenue.com';
+    private string $sandboxUrl = 'https://test.ccavenue.com';
+
+    public function __construct(
+        public RequestData $paymentData,
+        public ?string     $merchantId = null,
+        public ?string     $workingKey = null,
+        public ?string     $accessCode = null
+    ) {
     }
 
-    private function getUrl() : string
+    private function getUrl(): string
     {
         return config('ccavenue.environment') === 'sandbox' ? $this->sandboxUrl : $this->productionUrl;
     }
 
-    private function encryptRequestData() : string
+    private function encryptRequestData(): string
     {
         $data = '';
         foreach ($this->paymentData as $key => $value) {
             $data .= $key . '=' . $value . '&';
         }
-        $data.= 'merchant_id' . '=' . config('ccavenue.merchant_id') . '&';
-        $data.= 'language' . '=' . config('ccavenue.language') . '&';
-        $data.= 'currency' . '=' . config('ccavenue.currency') . '&';
-        $data.= 'integration_type=iframe_normal';
+        $data .= 'merchant_id' . '=' . $this->merchantId ?? config('ccavenue.merchant_id') . '&';
+        $data .= 'language' . '=' . config('ccavenue.language') . '&';
+        $data .= 'currency' . '=' . config('ccavenue.currency') . '&';
+        $data .= 'integration_type=iframe_normal';
 
-        return Crypto::encrypt($data, config('ccavenue.working_key'));
+        return Crypto::encrypt($data, $this->workingKey ?? config('ccavenue.working_key'));
     }
 
 
-    public function generatePaymentLink() : string
+    public function generatePaymentLink(): string
     {
         $link = $this->getUrl() . '/transaction/transaction.do';
         $link .= '?command=initiateTransaction';
         $link .= '&encRequest=' . $this->encryptRequestData();
-        $link .= '&access_code=' . config('ccavenue.access_code');
+        $link .= '&access_code=' . $this->accessCode ?? config('ccavenue.access_code');
         return $link;
     }
 }
