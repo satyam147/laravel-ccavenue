90c12b44ad313f1a7e4548f8c6501f0abe320a83
satyam147 <satyam161211149@gmail.com>
2024-07-24 20:31:43 +0530

    Added Required params

--- src/RequestData.php
+++ src/RequestData.php
@@ -4,6 +4,10 @@ namespace Satyam147\LaravelCcavenue;
 
 class RequestData
 {
+    public ?string $card_name = "NA";
+    public ?string $billing_address = "NA";
+    public ?string $billing_city = "NA";
+    public ?string $billing_zip = "NA";
     public string $order_id;
     public int $amount;
     public string $redirect_url;
