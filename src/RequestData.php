996b9e533df7815f103f877e1fc00818597442c5
satyam147 <satyam161211149@gmail.com>
2024-07-24 21:54:38 +0530

    Added Required params

--- src/RequestData.php
+++ src/RequestData.php
@@ -8,6 +8,10 @@ class RequestData
     public ?string $billing_address = "NA";
     public ?string $billing_city = "NA";
     public ?string $billing_zip = "NA";
+    public ?string $billing_state = "AP";
+    public ?string $billing_country = "India";
+    public ?string $billing_tel = "NA";
+    public ?string $billing_email = "NA";
     public string $order_id;
     public int $amount;
     public string $redirect_url;
